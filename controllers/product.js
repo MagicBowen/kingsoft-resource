205fdf52e221c2e21c375e96bec630a46804b9c4
Bowen <e.wangbo@aliyun.com>
2018-04-13 11:48:15 +0800

    add more

--- controllers/product.js
+++ controllers/product.js
@@ -3,6 +3,14 @@ var getProductPage = async (ctx, next) => {
     ctx.render('product.html', ctx.getProduct(id));
 };
 
+var getProduct =  async (ctx, next) => {
+    var id = ctx.query.id;
+    var product = ctx.getProduct(id);
+    ctx.response.type = "application/json";
+    ctx.response.body = product;
+};
+
 module.exports = {
-    'GET /product': getProductPage
+    'GET /product': getProductPage,
+    'GET /api/product': getProduct
 };
